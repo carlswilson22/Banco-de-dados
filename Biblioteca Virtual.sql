a056bae31e441b881a7db06042a6c4dc57f9df1e
Carlos Wilson <carlos.wilsong@sempreceub.com>
2025-05-11 11:11:33 -0300

    Update Biblioteca Virtual.sql

--- Biblioteca Virtual.sql
+++ Biblioteca Virtual.sql
@@ -1,17 +1,17 @@
--- Criar o banco de dados
+
 CREATE DATABASE literatura_brasileira;
 
--- Selecionar o banco de dados
+
 USE literatura_brasileira;
 
--- Criar a tabela de autores
+
 CREATE TABLE autores (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     genero VARCHAR(50) NOT NULL
 );
 
--- Criar a tabela de livros
+
 CREATE TABLE livros (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE livros (
     autor_id INT
 );
 
--- Inserir dados dos autores
+
 INSERT INTO autores (nome, genero) VALUES 
 ('Machado de Assis', 'Romance'),
 ('Clarice Lispector', 'Romance'),
@@ -27,7 +27,7 @@ INSERT INTO autores (nome, genero) VALUES
 ('Chico Buarque', 'Música/Literatura'),
 ('Monteiro Lobato', 'Infantil');
 
--- Inserir dados dos livros
+
 INSERT INTO livros (titulo, ano_publicacao, autor_id) VALUES 
 ('Dom Casmurro', 1899, 1),
 ('A Hora da Estrela', 1977, 2),
@@ -35,7 +35,7 @@ INSERT INTO livros (titulo, ano_publicacao, autor_id) VALUES
 ('Budapeste', 2003, 4),
 ('O Sítio do Picapau Amarelo', 1920, 5);
 
--- Consulta para exibir os autores e seus livros
+
 SELECT a.nome AS autor, a.genero, l.titulo AS livro, l.ano_publicacao
 FROM autores a
 JOIN livros l ON a.id = l.autor_id;
